b73b9d35ac994d9f95ab0f3cd60231254cbf03bc
Steve <steve@Madone.local>
2017-07-03 14:06:09 -0700

    initial checkin

---
+++ main.js
@@ -0,0 +1,12 @@
+const electron = require('electron')
+const {app, BrowserWindow} = electron
+
+app.on('ready', () => {
+  let win = new BrowserWindow({width: 800, height: 600})
+  win.loadURL(`file://${__dirname}/index.html`)
+})
+
+exports.openWindow = () => {
+  let win = new BrowserWindow({width:400, height:200})
+  win.loadURL(`file://${__dirname}/bear.html`)
+}
